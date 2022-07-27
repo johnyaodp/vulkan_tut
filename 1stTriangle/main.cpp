9121020e46f6a80097bc12812f5da282a195b9d1
JohnY <john.yao@datapath.co.uk>
2022-07-27 15:12:29 +0100

    create vulkan instance

--- 1stTriangle/main.cpp
+++ 1stTriangle/main.cpp
@@ -34,7 +34,7 @@ private:
    }
 
    void initVulkan() {
-
+      createInstance();
    }
 
    void mainLoop() {
@@ -44,13 +44,44 @@ private:
    }
 
    void cleanup() {
+      vkDestroyInstance(instance, nullptr);
+
       glfwDestroyWindow(window);
 
       glfwTerminate();
    }
 
 
+   void createInstance() {
+      VkApplicationInfo appInfo{};
+      appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+      appInfo.pApplicationName = "Hello Triangle";
+      appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+      appInfo.pEngineName = "No Engine";
+      appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+      appInfo.apiVersion = VK_API_VERSION_1_0;
+
+      VkInstanceCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+      createInfo.pApplicationInfo = &appInfo;
+
+      uint32_t glfwExtensionCount = 0;
+      const char** glfwExtensions;
+      glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+      createInfo.enabledExtensionCount = glfwExtensionCount;
+      createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+      createInfo.enabledLayerCount = 0;
+
+      if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+         throw std::runtime_error("failed to create instance!");
+      }
+   }
+
+   // members
    GLFWwindow* window = nullptr;
+   VkInstance instance = nullptr;
 };
 
 int main() {
